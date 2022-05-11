278d65255008d7bb72145c8687bfdff64f430bc7
rarras <>
2022-05-11 23:14:43 +0300

    iseseisev ülesanne

---
+++ js/images.js
@@ -0,0 +1,14 @@
+const currentMainImage = document.querySelector(".gallery-image-4 img");
+const imagesToChooseFrom = document.querySelectorAll(".gallery img");
+
+
+imagesToChooseFrom.forEach(img => img.addEventListener('click', imageClick));
+
+function imageClick(e) {
+
+    console.log(myBox);
+    
+    imagesToChooseFrom.forEach(img => img.style.opacity = 1);
+    currentMainImage.src = e.target.src;
+    e.target.style.opacity = 0.5;
+}
